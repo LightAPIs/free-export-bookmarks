095f98cb502a8603a5fe29f48f8ebb8d3e0ddeb2
Light <47911535+LightAPIs@users.noreply.github.com>
2020-12-31 19:34:49 +0800

    chore(eslint): 移除多余的规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'prettier/prettier': 'error',
-    'no-console': 'off',
   },
 };
