02c90f4553b014b46310b0232a96807c4b9c333f
dragonish <no.web.developer@outlook.com>
2025-03-01 14:38:41 +0800

    ci(scripts): update the changelog script

--- .gitea/scripts/tag-changelog-config.cjs
+++ .gitea/scripts/tag-changelog-config.cjs
@@ -1,4 +1,45 @@
 module.exports = {
-  types: [],
-  excludeTypes: ['docs', 'style', 'test', 'chore', 'build', 'ci', 'refactor', 'types', 'typings', 'typo'],
+  types: [
+    {
+      types: ['feat'],
+      label: 'Features',
+    },
+    {
+      types: ['fix'],
+      label: 'Bug Fixes',
+    },
+    {
+      types: ['perf'],
+      label: 'Performance Improvements',
+    },
+    {
+      types: ['enh'],
+      label: 'Enhancements',
+    },
+    {
+      types: ['tweak'],
+      label: 'Refinements',
+    },
+    {
+      types: ['adjust'],
+      label: 'Adjustments',
+    },
+    {
+      types: ['simplify'],
+      label: 'Simplifications',
+    },
+    {
+      types: ['deprecate'],
+      label: 'Deprecations',
+    },
+    {
+      types: ['ui'],
+      label: 'UI Improvements',
+    },
+    {
+      types: ['security'],
+      label: 'Security Fixes',
+    },
+  ],
+  excludeTypes: ['refactor', 'test', 'docs', 'typo', 'style', 'types', 'chore', 'config', 'build', 'ci', 'revert', 'init', 'merge'],
 };
